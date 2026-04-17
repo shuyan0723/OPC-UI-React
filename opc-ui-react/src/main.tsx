f5edc802d82c4739f82e6d7b57e82cbac97d8bdc
保管大王 <15264103+custody-king@user.noreply.gitee.com>
2026-04-17 16:23:58 +0800

    ! 非空断言符导致解析错误，移除 ! 符号

--- opc-ui-react/src/main.tsx
+++ opc-ui-react/src/main.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import './assets/styles/styles.css';
 import App from './App';
 
-createRoot(document.getElementById('root')!.render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
